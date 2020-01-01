5bfe026e2c7a42bc98649432747af92cb7078f6f
sanderdebr <sander606@gmail.com>
2020-01-01 12:02:25 +0100

    update

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,3 +1,6 @@
+import app from 'firebase/app';
+import 'firebase/auth';
+
 const config = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: "note-app-771cb.firebaseapp.com",
@@ -7,3 +10,27 @@ const config = {
     messagingSenderId: "877590012303",
     appId: "1:877590012303:web:20a7110bbccbff3c136bfa"
 };
+
+
+class Firebase {
+    constructor() {
+        app.initializeApp(config);
+
+        this.auth = app.auth();
+    }
+
+    /* *** Auth API *** */
+    doCreateUserWithEmailAndPassword = (email, password) =>
+    this.auth.createUserWithEmailAndPassword(email, password);
+
+    doSignInWithEmailAndPassword = (email, password) =>
+    this.auth.signInWithEmailAndPassword(email, password);
+
+    doSignOut = () => this.auth.signOut();
+
+    doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+    doPasswordUpdate = password =>
+        this.auth.currentUser.updatePassword(password);
+}
+export default Firebase;
